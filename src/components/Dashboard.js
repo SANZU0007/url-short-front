740a05acc489a09c8b9313ceb9112eeab11981f9
SANZU0007 <123228205+SANZU0007@users.noreply.github.com>
2024-02-05 14:17:47 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,41 +4,9 @@ import { useNavigate } from "react-router-dom";
 import URLShortener from "./urlApp/Url";
 
 const Dashboard = () => {
-  const { logindata, setLoginData } = useContext(LoginContext);
-  // console.log(logindata)
+ 
 
-  const history = useNavigate();
-  const checkUserValidity = async () => {
-    let token = localStorage.getItem("usersdatatoken");
-
-    try {
-      const res = await fetch("https://login-zft1.onrender.com/validuser", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-        },
-      });
-
-      const data = await res.json();
-
-      if (data.status === 401 || !data) {
-        
-      } else {
-        console.log("User verified");
-       setLoginData(data)
-   
-       
-      }
-    } catch (error) {
-      console.error("Error checking user validity:", error);
-      history("/error")
-    }
-  };
-
-  useEffect(() => {
-    checkUserValidity();
-  }, []);
+ 
 
   
   return (
