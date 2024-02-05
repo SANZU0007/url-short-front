ca4aae03498ec14ef47e45ea11f58b4c00068710
SANZU0007 <123228205+SANZU0007@users.noreply.github.com>
2024-02-05 14:34:40 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
                 <MenuItem
                   onClick={() => {
                     handleClose();
-                    goError();
+               
                   }}
                 >
                   Profile
